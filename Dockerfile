6543328c92227f5169ebaa0388c72b74042d108d
xerra <zbbergma@gmail.com>
2017-06-01 13:30:24 -0500

    (prog) Reattempt deploy of FE

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:7
+RUN mkdir /langApp
+ADD . /langApp
+WORKDIR /langApp
+RUN npm i
+EXPOSE 80
+CMD ["npm", "start"]
+
+
--- index.js
+++ index.js
@@ -5,4 +5,4 @@ server.get('/', (req, res) => res.sendFile(__dirname + "/public/www/index.html")
 
 server.use(express.static(__dirname + '/public/www/'));
 
-server.listen(3000, () => console.log('up and serving your front end on port 80'));
+server.listen(8080, () => console.log('up and serving your front end on port 8080'));
